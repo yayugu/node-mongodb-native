f024859536f5f5bf7d506b9f577cb35ee08c5fae
Christian Kvalheim <christkv@gmail.com>
2013-07-01 14:41:01 +0200

    Started trying out the fluent api

--- test/tests/functional/fluent_api_tests.js
+++ test/tests/functional/fluent_api_tests.js
@@ -4,25 +4,142 @@
 exports['should correctly perform simple insert with default write concern'] = function(configuration, test) {
   var db = configuration.db();
   var col = db.collection('fluent_api');
+  
   // Simple insert
   col.insert({a:1}, function(err, result) {
     test.equal(null, err);
-
-    db.close();
     test.done();
   });
 }
 
-// exports['start with withWriteConcern and perform insert'] = function(configuration, test) {
-//   var db = configuration.db();
-//   var col = db.collection('fluent_api');
-//   // Simple insert
-//   col
-//     .withWriteConcern({j:true})
-//     .insert({a:1}, function(err, result) {
-//       test.equal(null, err);
+exports['start with withWriteConcern and perform insert'] = function(configuration, test) {
+  var db = configuration.db();
+  var col = db.collection('fluent_api');
 
-//       db.close();
-//       test.done();
-//     });
-// }
+  // Insert with write concern
+  col
+    .withWriteConcern({j:true})
+    .insert({a:1}, function(err, result) {
+      test.equal(null, err);
+      test.done();
+    });
+}
+
+exports['start with withWriteConcern and perform save'] = function(configuration, test) {
+  var db = configuration.db();
+  var col = db.collection('fluent_api');
+
+  // With write concern
+  col
+    .withWriteConcern({j:true})
+    .save({a:1}, function(err, result) {
+      test.equal(null, err);
+      test.done();
+    });
+}
+
+exports['start with withWriteConcern and perform limit, isolate and update'] = function(configuration, test) {
+  var db = configuration.db();
+  var col = db.collection('fluent_api');
+
+  // Simple insert
+  col.insert([{g:1}, {g:1}], function(err, result) {
+    test.equal(null, err);
+
+    // With write concern
+    col
+      .withWriteConcern({j:true})
+      .limit(10)
+      .isolated()
+      .find({g:1})
+      .update({$set:{c:1}}, function(err, result) {
+        test.equal(null, err);
+        test.equal(true, result.updatedExisting);
+        test.equal(2, result.n);
+        test.done();
+      });
+  });
+}
+
+exports['start with withWriteConcern and perform single document update'] = function(configuration, test) {
+  var db = configuration.db();
+  var col = db.collection('fluent_api');
+
+  // Simple insert
+  col.insert({b:1}, function(err, result) {
+    test.equal(null, err);
+
+    // With write concern
+    col
+      .withWriteConcern({j:true})
+      .find({b:1})
+      .updateOne({$set:{c:1}}, function(err, result) {
+        test.equal(null, err);
+        test.equal(true, result.updatedExisting);
+        test.equal(1, result.n);
+        test.done();
+      });
+  });
+}
+
+exports['start with withWriteConcern and perform single updateOneAndGet'] = function(configuration, test) {
+  var db = configuration.db();
+  var col = db.collection('fluent_api');
+
+  // Simple insert
+  col.insert({d:1}, function(err, result) {
+    test.equal(null, err);
+
+    // With write concern
+    col
+      .withWriteConcern({w:1})
+      .find({d:1})
+      .updateOneAndGet({$set:{e:1}}, function(err, doc) {
+        test.equal(null, err);
+        test.equal(1, doc.e);
+        test.done();
+      });
+  });
+}
+
+exports['start with withWriteConcern and perform single updateOneAndGetOriginal'] = function(configuration, test) {
+  var db = configuration.db();
+  var col = db.collection('fluent_api');
+
+  // Simple insert
+  col.insert({d:1}, function(err, result) {
+    test.equal(null, err);
+
+    // With write concern
+    col
+      .withWriteConcern({w:1})
+      .find({d:1})
+      .updateOneAndGetOriginal({$set:{e:1}}, function(err, doc) {
+        test.equal(null, err);
+        test.equal(null, doc.e);
+        test.equal(1, doc.d);
+        test.done();
+      });
+  });
+}
+
+exports['start with withWriteConcern and perform single replaceOneAndGet'] = function(configuration, test) {
+  var db = configuration.db();
+  var col = db.collection('fluent_api');
+
+  // Simple insert
+  col.insert({d:1}, function(err, result) {
+    test.equal(null, err);
+
+    // With write concern
+    col
+      .withWriteConcern({w:1})
+      .find({d:1})
+      .replaceOneAndGet({d:2, e:10}, function(err, doc) {
+        test.equal(null, err);
+        test.equal(10, doc.e);
+        test.equal(2, doc.d);
+        test.done();
+      });
+  });
+}
