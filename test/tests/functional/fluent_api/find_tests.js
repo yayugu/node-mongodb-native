4d7575460346c2c8e4c42d269faa5fb7caceb7bf
Christian Kvalheim <christkv@gmail.com>
2013-07-11 12:17:44 +0200

    Added some more functions for the fluent api

--- test/tests/functional/fluent_api/find_tests.js
+++ test/tests/functional/fluent_api/find_tests.js
@@ -83,3 +83,131 @@ exports['Should correctly execute count'] = function(configuration, test) {
       test.done();
     });
 }
+
+exports['Should correctly perform a simple getOne using fields to limit returned fields'] = function(configuration, test) {
+  var ReadPreference = configuration.getMongoPackage().ReadPreference;
+  var db = configuration.db();
+  var col = db.collection('fluent_api');
+
+  // Simple insert
+  col.insert([{fields:1, b:1}], function(err, result) {
+    test.equal(null, err);
+
+    // With write concern
+    col
+      .find({fields: {$gt: 0}})
+      .sort({fields: -1})
+      .fields({b:1})
+      .getOne(function(err, doc) {
+        test.equal(undefined, doc.fields);
+        test.equal(1, doc.b);
+        test.done();
+      });
+  });
+}
+
+exports['Should correctly execute query explain using hint'] = function(configuration, test) {
+  var ReadPreference = configuration.getMongoPackage().ReadPreference;
+  var db = configuration.db();
+  var col = db.collection('fluent_api');
+
+  // Simple insert
+  col.insert([{hint:1, b:1}], function(err, result) {
+    test.equal(null, err);
+
+    col.ensureIndex({hint:1}, function(err, result) {
+      // With write concern
+      col
+        .find({findOne: {$gt: 0}})
+        .withReadPreference(ReadPreference.SECONDARY)    
+        .limit(1)
+        .skip(1)
+        .hint({_id:1})
+        .sort({hint: -1})
+        .explain(function(err, doc) {
+          test.equal(null, err);
+          test.ok(doc.cursor.indexOf('_id_') != -1);
+          test.done();
+        });
+    });
+  });
+}
+
+exports['Should correctly execute query explain using maxScan'] = function(configuration, test) {
+  var ReadPreference = configuration.getMongoPackage().ReadPreference;
+  var db = configuration.db();
+  var col = db.collection('fluent_api');
+
+  // Insert 100 documents
+  var docs = [];
+  for(var i = 0; i < 100; i++) {
+    docs.push({maxScan:1, i: i});
+  }
+
+  // Simple insert
+  col.insert(docs, function(err, result) {
+    test.equal(null, err);
+
+    col.ensureIndex({maxScan:1}, function(err, result) {
+      // With write concern
+      col
+        .find({maxScan: 1})
+        .maxScan(10)
+        .maxTime(1000)
+        .get(function(err, docs) {
+          test.equal(null, err);
+          test.equal(10, docs.length);
+          test.done();
+        });
+    });
+  });
+}
+
+exports['Should correctly execute query explain using min and max'] = function(configuration, test) {
+  var ReadPreference = configuration.getMongoPackage().ReadPreference;
+  var db = configuration.db();
+  var col = db.collection('fluent_api');
+
+  // Insert 100 documents
+  var docs = [
+      { "_id" : 6, "item" : "apple", "type" : "cortland", "price" : 1.29 }
+    , { "_id" : 2, "item" : "apple", "type" : "fuji", "price" : 1.99 }
+    , { "_id" : 1, "item" : "apple", "type" : "honey crisp", "price" : 1.99 }
+    , { "_id" : 3, "item" : "apple", "type" : "jonagold", "price" : 1.29 }
+    , { "_id" : 4, "item" : "apple", "type" : "jonathan", "price" : 1.29 }
+    , { "_id" : 5, "item" : "apple", "type" : "mcintosh", "price" : 1.29 }
+    , { "_id" : 7, "item" : "orange", "type" : "cara cara", "price" : 2.99 }
+    , { "_id" : 10, "item" : "orange", "type" : "navel", "price" : 1.39 }
+    , { "_id" : 9, "item" : "orange", "type" : "satsuma", "price" : 1.99 }
+    , { "_id" : 8, "item" : "orange", "type" : "valencia", "price" : 0.99 }    
+  ];
+
+  // Simple insert
+  col.insert(docs, function(err, result) {
+    test.equal(null, err);
+
+    col.ensureIndex({ "item" : 1, "type" : 1 }, function(err, result) {
+      test.equal(null, err);
+
+      col.ensureIndex({ "item" : 1, "type" : -1 }, function(err, result) {
+        test.equal(null, err);
+
+        col.ensureIndex({ "prices" : 1}, function(err, result) {
+          test.equal(null, err);
+
+          // With write concern
+          col
+            .find()
+            .min( { item: 'apple', type: 'jonagold' } )
+            .max( { item: 'apple', type: 'navel' } )
+            .hint( { item: 1, type: 1 } )
+            .get(function(err, docs) {
+              test.equal(null, err);
+              test.equal(3, docs.length);
+              test.done();
+            });
+        });
+      });
+    });
+  });
+}
